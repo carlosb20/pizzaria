423ec1ecc8ee5b21a06ecf969697895394b2313a
carlos alberto <carlosalbertosnb20@gmail.com>
2026-06-22 20:17:46 -0300

    dd

--- backend/public/script.js
+++ backend/public/script.js
@@ -1,7 +1,8 @@
 
 const app = async () => {
     const resposta = await fetch(
-        "https://pizzaria-production-299a.up.railway.app/"
+        "https://pizzaria-production-299a.up.railway.app/api/pizzas"
+       
     );
 
     const dados = await resposta.json();
