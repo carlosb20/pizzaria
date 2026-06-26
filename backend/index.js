a87771af90d9ddb1e533b938f48f7ffc2bee4c83
carlos alberto <carlosalbertosnb20@gmail.com>
2026-06-25 23:51:20 -0300

    atualizado

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,21 @@
 const express = require("express")
 const app = express()
-
-// -const PORT = 3000
-
 const cardapio = require("./dados/cardapio");
-app.use("/imagem", express.static("imagem"));
 const cors = require('cors');
 
 app.use(cors());
-
+app.use("/imagem", express.static("imagem"));
 app.use(express.json());
 app.use(express.static("public"));
 
-
-
+// ver todos os itens
 app.get("/api/pizzas", (req, res) => {
     res.json(cardapio);
 });
 
+
 const PORT = process.env.PORT || 3000;
 
-// app.get("/pizza",(req,res)=>{
-
-//     res.json(cardapio.pizzas)
-// })
-
-// app.get("/refrigerantes",(req,res)=>{
-
-//     res.json(cardapio.refrigerantes)
-// })
 
 // app.get("/pizza/:id", (req, res) => {
 //     const id = Number(req.params.id);
--- backend/public/script.js
+++ backend/public/script.js
@@ -1,13 +1,21 @@
 
-const app = async () => {
-    const resposta = await fetch(
-        "https://pizzaria-production-299a.up.railway.app/api/pizzas"
-       
-    );
+const img = document.getElementById("img")
 
+const app = async () => {
+    const resposta = await fetch("https://pizzaria-production-299a.up.railway.app/api/pizzas");
     const dados = await resposta.json();
 
-    console.log(dados.pizzas);
+    console.log(dados.refrigerantes[0]);
+
+
+    const val = `
+    
+    https://pizzaria-production-299a.up.railway.app/imagem/${dados.refrigerantes[1].foto}`
+
+    img.src = val
+  
+
+    console.log(val)
 };
 
 app();
