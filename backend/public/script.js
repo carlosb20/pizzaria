e0e34f094b5966903bbf314835a19c9b9efe4c16
carlos alberto <carlosalbertosnb20@gmail.com>
2026-06-26 21:13:16 -0300

    fdf

--- backend/public/script.js
+++ backend/public/script.js
@@ -3,16 +3,16 @@ const img = document.getElementById("img")
 const id_p = document.getElementById("num")
 const btn = document.getElementById("btn")
 
-const app = async (vals) => {
+const app = async () => {
     const resposta = await fetch("https://pizzaria-production-299a.up.railway.app/api/pizzas");
     const dados = await resposta.json();
 
-    console.log(dados.refrigerantes[vals]);
+    console.log(dados.refrigerantes[1]);
 
 
     const val = `
     
-    https://pizzaria-production-299a.up.railway.app/imagem/${dados.refrigerantes[vals].foto}`
+    https://pizzaria-production-299a.up.railway.app/imagem/${dados.refrigerantes[1].foto}`
 
     img.src = val
   
@@ -20,10 +20,11 @@ const app = async (vals) => {
     console.log(val)
 
 };
+app()
 
-btn.addEventListener("click",()=>{
+// btn.addEventListener("click",()=>{
 
-    app(Number(id_p.value))
+//     app(Number(id_p.value))
 
-})
+// })
 
