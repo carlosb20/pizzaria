bdace9eb0a93e0459f5e3d1f76efe0ab9f77be08
carlos alberto <carlosalbertosnb20@gmail.com>
2026-07-04 21:40:18 -0300

    rr

--- backend/public/script.js
+++ backend/public/script.js
@@ -7,10 +7,14 @@ const setPizzas = async () => {
     const resposta = await fetch("https://pizzaria-production-299a.up.railway.app/api/pizzas");
     const dados = await resposta.json();
 
-    console.log(dados.refrigerantes[1]);
+
 
 
     const val = ` https://pizzaria-production-299a.up.railway.app/imagem/${dados.refrigerantes[8].foto}`
+
+
+
+
 
    
     img.src = val
