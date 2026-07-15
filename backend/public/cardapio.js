33c15697fbb64649f9037de90bbbc3fbc5da4d96
carlos alberto <carlosalbertosnb20@gmail.com>
2026-07-15 20:02:43 -0300

    ddd

--- backend/public/cardapio.js
+++ backend/public/cardapio.js
@@ -5,7 +5,7 @@ class Carrinho {
     constructor() {
 
         this.produtos = [];
-        this.main = document.getElementById("main")
+        this.section = document.getElementById("cardapio-caixa")
         this.cont_carrinho = document.querySelector(".carrinho > p")
     }
     async lista_cardapio() {
@@ -15,22 +15,26 @@ class Carrinho {
         dados.pizzas.map((e) => {
 
             const contCaixa = document.createElement("div")
+            contCaixa.setAttribute("class", 'caixa-itens')
             const img = document.createElement("img")
             const logoImg = `https://pizzaria-production-299a.up.railway.app/imagem/${e.foto}`
             const div_txt = document.createElement('div')
+            div_txt.setAttribute("class", 'caixa-itens2')
             const p = document.createElement("p");
             const p2 = document.createElement("p");
 
-            img.src = `${logoImg}`
-            img.style.width = '12em'
-            img.style.height = '12em'
+            div_txt.style.width = '100px'
 
-            this.main.appendChild(contCaixa)
-            this.main.style.display = 'flex'
-            this.main.style.flexWrap = 'wrap'
-            this.main.style.gap = '0.4em'
-            this.main.style.padding = '0.5em'
-            this.main.style.justifyContent = 'center'
+            img.src = `${logoImg}`
+            img.style.width = '6em'
+            img.style.height = '10em'
+
+            this.section.appendChild(contCaixa)
+            this.section.style.display = 'flex'
+            this.section.style.flexWrap = 'wrap'
+            this.section.style.gap = '0.4em'
+            this.section.style.padding = '0.5em'
+            this.section.style.justifyContent = 'center'
 
             contCaixa.appendChild(img)
             contCaixa.style.border = '1px solid red'
@@ -52,34 +56,60 @@ class Carrinho {
                 const exixte = this.produtos.find(p => p.id === e.id);
                 if (!exixte) {
 
-                    this.produtos.push({...e,quant :1});
+                    this.produtos.push({ ...e, quant: 1 });
                     this.numeroPedidos();
                 }
-
             });
         })
     }
 
     numeroPedidos() {
-       
+
         if (this.produtos.length > 0) {
             const total = this.produtos.length;
-            this.cont_carrinho.textContent = `${total}`;    
+            this.cont_carrinho.textContent = `${total}`;
         }
     }
 
-    vizualizarCarrinho(){
+    vizualizarCarrinho() {
         const ver_carrinho = document.getElementById("carrinho_")
         const contCaixa_ = document.getElementById("main")
+         const p_c = document.getElementById("p_c")
         const verCarro = document.querySelector(".verCarro")
+        const verdadosCarrinho = document.createElement("div")
 
-        ver_carrinho.addEventListener('click',()=>{
-            console.log(verCarro)
-            contCaixa_.style.display = 'none'  
+        ver_carrinho.addEventListener('click', () => {
+             
+            if (this.section.style.display === 'none') {
+                this.section.style.display = 'flex';
+                verCarro.style.display = 'none';
+               
+                verdadosCarrinho.style.width = '150px'
+                verdadosCarrinho.style.height = '150px'
+                verdadosCarrinho.textContent = 'dds'
+                verdadosCarrinho.style.backgroundColor = 'blue'
+                
+                // verCarro.appendChild(verdadosCarrinho)
+                
+
+            } else {
+                this.section.style.display = 'none';
+                verCarro.style.display = 'flex';
+                
+            }
+
+            p_c.addEventListener('click', () => {
+                this.section.style.display = 'flex';
+                verCarro.style.display = 'none';
+                 
+                console.log('sasa')
+            })
         })
+
+        
     }
 
-    soma(){
+    soma() {
         console.log('pass')
     }
 }
